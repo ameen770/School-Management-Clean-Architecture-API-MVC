dbe10602cd54a45be7906445448b3b7079c75f46
ameen.hameed.2022 <ameen.hameed.2022@hotmail.com>
2023-11-28 23:43:28 +0300

    add new project called WebUI to get data from API

---
+++ SchoolProject.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SchoolProject.WebUI.Models;
+
+public class HomeController : Controller
+{
+    private readonly HttpClient httpClient;
+    private string BaseURL = "https://localhost:7136/Student/List";
+
+    public HomeController()
+    {
+        httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri("https://localhost:7136/Student/List"); // Replace with your Web API base URL
+    }
+
+    public async Task<ActionResult> Index()
+    {
+        List<Student> students = new List<Student>();
+
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(BaseURL); // Replace with your Web API endpoint URL
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                students = JsonConvert.DeserializeObject<List<Student>>(responseContent);
+            }
+            else
+            {
+                // Handle the error response
+                // For example, you can log the error or display an error message
+                // response.StatusCode and response.ReasonPhrase provide additional information
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle the exception
+        }
+
+        return View(students);
+    }
+}
---
+++ SchoolProject.WebUI/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject.WebUI.Models
+{
+    public class Student
+    {
+        public int StudID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
